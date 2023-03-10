739ccde7bfbfb01f8a9f7021c499c3eb29d518d2
Pratik Gaikwad <pratik2000gaikwad@gmail.com>
2023-03-10 16:31:55 +0530

    connection to db

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/";
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI);
+    console.log("connected to mongo successfully");
+}
+// const connectToMongo = ()=>{
+//     mongoose.connect(mongoURI, ()=>{
+//         console.log("connected to Mongo Successfully");
+//     })
+// }
+
+module.exports = connectToMongo;
