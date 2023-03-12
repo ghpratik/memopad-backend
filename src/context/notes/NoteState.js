a7fdc42510796803b5f15ccee0467767889e3580
Pratik Gaikwad <pratik2000gaikwad@gmail.com>
2023-03-12 19:24:30 +0530

    update form in modal client server

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -36,7 +36,7 @@ const NoteState = (props) => {
             body: JSON.stringify({title, description, tag}), // body data type must match "Content-Type" header
         });
 
-        // client side delete
+        // client side add note
         const note = {
             "_id": "640d7df2632f7957a538ec4",
             "user": "640c548a276d5346f151e442",
@@ -52,23 +52,22 @@ const NoteState = (props) => {
     //Delete a note
 
     const deleteNote = async(id) => {
-        //API call
-        // const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-        //     method: "POST", // *GET, POST, PUT, DELETE, etc.
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQwYzU0OGEyNzZkNTM0NmYxNTFlNDQyIn0sImlhdCI6MTY3ODUzMDIyN30.KNG_9USbMU5UvwyKVEDj-VunPiFcGy5sLf5vvn69WqY"
-        //     },
-        //     redirect: "follow", // manual, *follow, error
-        //     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        //     body: JSON.stringify({data}), // body data type must match "Content-Type" header
-        // });
-
-        //const json = response.json(); // parses JSON response into native JavaScript objects
-        // client side delete
         console.log("deleting note with id " + id)
         const newNote = notes.filter((note) => { return note._id !== id })
         setNotes(newNote);
+        //API call
+
+        // client side delete
+        const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+            method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+            headers: {
+                "Content-Type": "application/json",
+                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQwYzU0OGEyNzZkNTM0NmYxNTFlNDQyIn0sImlhdCI6MTY3ODUzMDIyN30.KNG_9USbMU5UvwyKVEDj-VunPiFcGy5sLf5vvn69WqY"
+            },
+        });
+        const json = await response.json();
+        console.log(json);
+        json = response.json(); // parses JSON response into native JavaScript objects
     }
 
     //Edit a note
