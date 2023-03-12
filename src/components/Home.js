d436169068cd732d7b25cf3933b3b766282af016
Pratik Gaikwad <pratik2000gaikwad@gmail.com>
2023-03-12 13:36:54 +0530

    setting content in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Notes from './Notes';
 const Home = () => {
     return (
         <div>
@@ -22,10 +22,7 @@ const Home = () => {
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
-            <div className="container my-3">
-                <h2>Your Notes</h2>
-            </div>
-
+            <Notes/>
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,10 +1,102 @@
 import { useState } from "react";
 import noteContext from "./noteContext";
 
-const NoteState = (props)=>{
-   
+const NoteState = (props) => {
+    const notesInitial = [
+        {
+            "_id": "640c662f4a7cca612a0ab713",
+            "user": "640c548a276d5346f151e442",
+            "title": "My note Updated",
+            "description": "Please wake up early at 12pm",
+            "tag": "personality",
+            "date": "1678534191077",
+            "__v": 0
+        },
+        {
+            "_id": "640d7db2632f79467a438ec2",
+            "user": "640c548a276d5346f151e442",
+            "title": "My Note 2",
+            "description": "Please wake up early to study physics",
+            "tag": "study",
+            "date": "1678605746502",
+            "__v": 0
+        },
+        {
+            "_id": "640d7df2632f79467a438ec4",
+            "user": "640c548a276d5346f151e442",
+            "title": "My Note 3",
+            "description": "Buy grocery from below list",
+            "tag": "task to do",
+            "date": "1678605810767",
+            "__v": 0
+        },
+        {
+            "_id": "640d7df2632f79467a438ec4",
+            "user": "640c548a276d5346f151e442",
+            "title": "My Note 3",
+            "description": "Buy grocery from below list",
+            "tag": "task to do",
+            "date": "1678605810767",
+            "__v": 0
+        },
+        {
+            "_id": "640d7df2632f79467a438ec4",
+            "user": "640c548a276d5346f151e442",
+            "title": "My Note 3",
+            "description": "Buy grocery from below list",
+            "tag": "task to do",
+            "date": "1678605810767",
+            "__v": 0
+        },
+        {
+            "_id": "640d7df2632f79467a438ec4",
+            "user": "640c548a276d5346f151e442",
+            "title": "My Note 3",
+            "description": "Buy grocery from below list",
+            "tag": "task to do",
+            "date": "1678605810767",
+            "__v": 0
+        },
+        {
+            "_id": "640d7df2632f79467a438ec4",
+            "user": "640c548a276d5346f151e442",
+            "title": "My Note 3",
+            "description": "Buy grocery from below list",
+            "tag": "task to do",
+            "date": "1678605810767",
+            "__v": 0
+        },
+        {
+            "_id": "640d7df2632f79467a438ec4",
+            "user": "640c548a276d5346f151e442",
+            "title": "My Note 3",
+            "description": "Buy grocery from below list",
+            "tag": "task to do",
+            "date": "1678605810767",
+            "__v": 0
+        },
+        {
+            "_id": "640d7df2632f79467a438ec4",
+            "user": "640c548a276d5346f151e442",
+            "title": "My Note 3",
+            "description": "Buy grocery from below list",
+            "tag": "task to do",
+            "date": "1678605810767",
+            "__v": 0
+        },
+        {
+            "_id": "640d7df2632f79467a438ec4",
+            "user": "640c548a276d5346f151e442",
+            "title": "My Note 3",
+            "description": "Buy grocery from below list",
+            "tag": "task to do",
+            "date": "1678605810767",
+            "__v": 0
+        }
+    ];
+    const [notes, setNotes] = useState(notesInitial)
     return (
-        <noteContext.Provider value={{}}>
+        <noteContext.Provider value={{ notes, setNotes }}>
             {props.children}
         </noteContext.Provider>
     )
