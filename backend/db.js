9f968b37f90b70f16647e64cd73376aaed274acf
Pratik Gaikwad <pratik2000gaikwad@gmail.com>
2023-03-18 01:15:00 +0530

    connection to mongo db in atlas cloud

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook";
+// const mongoURI = "mongodb://localhost:27017/inotebook";/
+const mongoURI = "mongodb+srv://pratikgaikwad:Pratik2000@cluster0.48lsq0q.mongodb.net/iNotebook";
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI);
-    console.log("connected to mongo successfully");
+    mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(()=> console.log("connected to MongoDB successfully"))
+    .catch((error)=> console.log("MongoDB connection failed: ", error.message))
+    // console.log("connected to mongo successfully");
 }
 // const connectToMongo = ()=>{
 //     mongoose.connect(mongoURI, ()=>{
